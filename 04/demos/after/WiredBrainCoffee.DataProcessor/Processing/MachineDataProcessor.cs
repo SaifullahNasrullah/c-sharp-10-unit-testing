dcf10fd9b95a6539ac01106780d8c7885da5e49b
saifullah <hamraz.saifullah@gmail.com>
2023-11-05 13:28:34 +0100

    first commit

---
+++ 04/demos/after/WiredBrainCoffee.DataProcessor/Processing/MachineDataProcessor.cs
@@ -0,0 +1,48 @@
+﻿using WiredBrainCoffee.DataProcessor.Data;
+using WiredBrainCoffee.DataProcessor.Model;
+
+namespace WiredBrainCoffee.DataProcessor.Processing
+{
+    public class MachineDataProcessor
+    {
+        private readonly Dictionary<string, int> _countPerCoffeeType = new();
+        private readonly ICoffeeCountStore _coffeeCountStore;
+
+        public MachineDataProcessor(ICoffeeCountStore coffeeCountStore)
+        {
+            _coffeeCountStore = coffeeCountStore;
+        }
+
+        public void ProcessItems(MachineDataItem[] dataItems)
+        {
+            _countPerCoffeeType.Clear();
+
+            foreach (var dataItem in dataItems)
+            {
+                ProcessItem(dataItem);
+            }
+
+            SaveCountPerCoffeeType();
+        }
+
+        private void ProcessItem(MachineDataItem dataItem)
+        {
+            if (!_countPerCoffeeType.ContainsKey(dataItem.CoffeeType))
+            {
+                _countPerCoffeeType.Add(dataItem.CoffeeType, 1);
+            }
+            else
+            {
+                _countPerCoffeeType[dataItem.CoffeeType]++;
+            }
+        }
+
+        private void SaveCountPerCoffeeType()
+        {
+            foreach (var entry in _countPerCoffeeType)
+            {
+                _coffeeCountStore.Save(new CoffeeCountItem(entry.Key, entry.Value));
+            }
+        }
+    }
+}
